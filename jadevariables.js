941aa65300ee4cbe44abd8bf35f7a1ac48717082
Kirk Longuski <longuskikirk@gmail.com>
2018-12-31 12:32:49 -0500

    update links

--- jadevariables.js
+++ jadevariables.js
@@ -6,7 +6,7 @@ module.exports.jadevars = {
             'href':'./'
         }, {
             'title': 'resume',
-            'href': '//drive.google.com/file/d/0B0bcseZsc82KZ0Vrb0JvNFAzMms/view?usp=sharing'
+            'href': '//drive.google.com/file/d/1VBmezzae2BHA52Iff2zDaoNC5xp1SBYb/view?usp=sharing'
         }, {
             'title': 'reddit',
             'href': '//reddit.com/u/k_longuski'
